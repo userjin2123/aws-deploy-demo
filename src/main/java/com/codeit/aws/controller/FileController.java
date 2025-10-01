8b920bf2f6a68a997d2b6898ba98dd8e8aa49794
userjin2123 <userjin2123@gmail.com>
2025-10-01 11:01:38 +0900

    init commit 2

--- src/main/java/com/codeit/aws/controller/FileController.java
+++ src/main/java/com/codeit/aws/controller/FileController.java
@@ -28,6 +28,7 @@ public class FileController {
         log.info("Received file upload request: filename={}, description={}",
                 file.getOriginalFilename(), description);
 
+        log.info("@@");
         FileResponseDto response = fileService.uploadFile(file, description);
         return ResponseEntity.ok(response);
     }
